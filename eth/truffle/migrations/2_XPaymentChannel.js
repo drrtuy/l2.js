1f0b1d2283fa33257179dabe38c3b35ce3746d9b
Roman Nozdrin <drrtuy@gmail.com>
2018-05-26 21:52:13 +0300

    WIP Prepare the hedgehog(aka example1.js) for the flight. Truffle migrations copy.

---
+++ eth/truffle/migrations/2_XPaymentChannel.js
@@ -0,0 +1,15 @@
+var XPaymentChannel = artifacts.require("XPaymentChannel");
+
+module.exports = function(deployer, network, accounts) {
+  //XPaymentChannel.defaults({ value: web3.toWei(5.0, "ether") });
+  var value = web3.toWei(5.0, "ether");
+  deployer.deploy(XPaymentChannel, accounts[2], 255, 400000, {value: value});
+  XPaymentChannel.deployed().
+  then(instance => {
+      //var instance = XPaymentChannel.at(XPaymentChannel.address);
+      instance.A().then(result => console.log('A() '+ result));
+      instance.B().then(result => console.log('B() '+ result));
+      instance.sum().then(result => console.log('sum() '+ result));
+      instance.state().then(result => console.log('state() '+ result));
+  })
+};
